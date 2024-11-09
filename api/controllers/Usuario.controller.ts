73a90eb97d1266f8f5f0427f4d2c62ed377566e6
IagoACPereira <iago0307@gmail.com>
2024-11-09 19:10:55 -0300

    feat: :wrench: add: Estrutura inicial de controllers e rotas

---
+++ api/controllers/Usuario.controller.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+
+export class UsuarioController {
+  async adicionar(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      res.status(200).json({
+        mensagem: 'Em desenvolvimento',
+      });
+    } catch (error) {
+      res.status(400).json({
+        mensagem: `${error}`,
+      });
+    }
+  }
+
+  async exibirTodos(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      res.status(200).json({
+        mensagem: 'Em desenvolvimento',
+      });
+    } catch (error) {
+      res.status(400).json({
+        mensagem: `${error}`,
+      });
+    }
+  }
+
+  async exibirUm(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      res.status(200).json({
+        mensagem: 'Em desenvolvimento',
+      });
+    } catch (error) {
+      res.status(400).json({
+        mensagem: `${error}`,
+      });
+    }
+  }
+
+  async atualizar(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      res.status(200).json({
+        mensagem: 'Em desenvolvimento',
+      });
+    } catch (error) {
+      res.status(400).json({
+        mensagem: `${error}`,
+      });
+    }
+  }
+
+  async deletar(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      res.status(200).json({
+        mensagem: 'Em desenvolvimento',
+      });
+    } catch (error) {
+      res.status(400).json({
+        mensagem: `${error}`,
+      });
+    }
+  }
+}
--- api/routes/index.ts
+++ api/routes/index.ts
@@ -3,6 +3,12 @@ import pais from './Pais.router';
 import regiao from './Regiao.router';
 import genero from './Genero.router';
 import artista from './Artista.router';
+import disco from './Disco.router';
+import faixa from './Faixa.router';
+import gravadora from './Gravadora.router';
+import permissao from './Permissao.router';
+import tipo from './Tipo.router';
+import usuario from './Usuario.router';
 
 
 export function routes(app: Router) {
@@ -10,8 +16,14 @@ export function routes(app: Router) {
     .get('/forja-bruta/api', (req: Request, res: Response<string>) => {
       res.status(200).json('Forja Bruta API')
     })
+    .use('/forja-bruta/api/artista', artista)
     .use('/forja-bruta/api/pais', pais)
     .use('/forja-bruta/api/regiao', regiao)
     .use('/forja-bruta/api/genero', genero)
-    .use('/forja-bruta/api/artista', artista);
+    .use('/forja-bruta/api/disco', disco)
+    .use('/forja-bruta/api/faixa', faixa)
+    .use('/forja-bruta/api/gravadora', gravadora)
+    .use('/forja-bruta/api/permissao', permissao)
+    .use('/forja-bruta/api/tipo', tipo)
+    .use('/forja-bruta/api/usuario', usuario);
 }

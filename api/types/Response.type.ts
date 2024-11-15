69c8c0ed74dc101b80a96458377b3c9814b8bd3e
IagoACPereira <iago0307@gmail.com>
2024-11-15 16:00:25 -0300

    refactor: :label: add: Tipos para response

--- api/types/Response.type.ts
+++ api/types/Response.type.ts
@@ -1,6 +1,13 @@
+import { TArtista } from "./Artista.type";
+import { TDisco } from "./Disco.type";
+import { TFaixa } from "./Faixa.type";
 import { TGenero } from "./Genero.type";
+import { TGravadora } from "./Gravadora.type";
 import { TPais } from "./Pais.type";
+import { TPermissao } from "./Permissao.type";
 import { TRegiao } from "./Regiao.type";
+import { TTipo } from "./Tipo.type";
+import { TUsuario } from "./Usuario.type";
 
 export type TResponseErro = {
   mensagem: string;
@@ -25,6 +32,48 @@ export namespace TResponsePost {
     dados: TGenero;
     statusCode: number;
   };
+
+  export type Artista = {
+    mensagem: string;
+    dados: TArtista;
+    statusCode: number;
+  };
+
+  export type Gravadora = {
+    mensagem: string;
+    dados: TGravadora;
+    statusCode: number;
+  };
+
+  export type Tipo = {
+    mensagem: string;
+    dados: TTipo;
+    statusCode: number;
+  };
+
+  export type Disco = {
+    mensagem: string;
+    dados: TDisco;
+    statusCode: number;
+  };
+
+  export type Faixa = {
+    mensagem: string;
+    dados: TFaixa;
+    statusCode: number;
+  };
+
+  export type Permissao = {
+    mensagem: string;
+    dados: TPermissao;
+    statusCode: number;
+  };
+
+  export type Usuario = {
+    mensagem: string;
+    dados: TUsuario;
+    statusCode: number;
+  };
 };
 
 export namespace TResponseGet {
@@ -42,6 +91,41 @@ export namespace TResponseGet {
     generos: Array<TGenero>;
     statusCode: number;
   };
+
+  export type Artista = {
+    generos: Array<TArtista>;
+    statusCode: number;
+  };
+
+  export type Gravadora = {
+    generos: Array<TGravadora>;
+    statusCode: number;
+  };
+
+  export type Tipo = {
+    generos: Array<TTipo>;
+    statusCode: number;
+  };
+
+  export type Disco = {
+    generos: Array<TDisco>;
+    statusCode: number;
+  };
+
+  export type Faixa = {
+    generos: Array<TFaixa>;
+    statusCode: number;
+  };
+
+  export type Permissao = {
+    generos: Array<TPermissao>;
+    statusCode: number;
+  };
+
+  export type Usuario = {
+    generos: Array<TUsuario>;
+    statusCode: number;
+  };
 };
 
 export namespace TResponseGetId {
@@ -57,6 +141,41 @@ export namespace TResponseGetId {
 
   export type Genero = {
     genero: TGenero;
+    statusCode: number;
+  };
+
+  export type Artista = {
+    genero: TArtista;
+    statusCode: number;
+  };
+
+  export type Gravadora = {
+    genero: TGravadora;
+    statusCode: number;
+  };
+
+  export type Tipo = {
+    genero: TTipo;
+    statusCode: number;
+  };
+
+  export type Disco = {
+    genero: TDisco;
+    statusCode: number;
+  };
+
+  export type Faixa = {
+    genero: TFaixa;
+    statusCode: number;
+  };
+
+  export type Permissao = {
+    genero: TPermissao;
+    statusCode: number;
+  };
+
+  export type Usuario = {
+    genero: TUsuario;
     statusCode: number;
   };
 };

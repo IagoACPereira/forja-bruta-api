dcdba69f5734ef29b8ea71571aae6696a74f89e1
IagoACPereira <iago0307@gmail.com>
2024-11-17 18:03:59 -0300

    feat: ✨ add: Controller para gravadoras

--- api/controllers/Gravadora.controller.ts
+++ api/controllers/Gravadora.controller.ts
@@ -1,77 +1,130 @@
 import { Request, Response } from "express";
+import { PaisModel } from "../models/Pais.model";
+import { 
+  TResponseDelete, 
+  TResponseErro, 
+  TResponseGet, 
+  TResponseGetId, 
+  TResponsePost, 
+  TResponsePut 
+} from "../types/Response.type";
+import { TRequestBody, TRequestParams } from "../types/Request.type";
+import { ArtistaModel } from "../models/Artista.model";
+import { TGravadora } from "../types/Gravadora.type";
+import { GravadoraModel } from "../models/Gravadora.model";
 
-export class GravadoraController {
+export class ArtistaController {
   async adicionar(
-    req: Request,
-    res: Response
+    req: Request<{},{},TRequestBody.Gravadora>,
+    res: Response<TResponsePost.Gravadora | TResponseErro>
   ): Promise<void> {
+    const { 
+      id_regiao,
+      nome,
+      url_imagem
+    } = req.body as TGravadora;
     try {
-      res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+      const novaGravadora = await new GravadoraModel(
+        undefined,
+        nome,
+        url_imagem,
+        id_regiao,
+      ).adicionar();
+
+      res.status(201).json({
+        mensagem: 'Gravadora adicionada com sucesso',
+        dados: novaGravadora,
+        statusCode: 201,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirTodos(
     req: Request,
-    res: Response
+    res: Response<TResponseGet.Gravadora | TResponseErro>
   ): Promise<void> {
     try {
+      const gravadoras = await new GravadoraModel().pegaTodos();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        gravadoras,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirUm(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Gravadora>,
+    res: Response<TResponseGetId.Gravadora | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      const gravadora = await new GravadoraModel(id).pegaUmPorId();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        gravadora,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async atualizar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Gravadora, {}, TRequestBody.Gravadora>,
+    res: Response<TResponsePut | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
+    const { 
+      id_regiao,
+      nome,
+      url_imagem,
+     } = req.body as TGravadora;
     try {
+      await new GravadoraModel(
+        id,
+        nome,
+        url_imagem,
+        id_regiao,
+      ).atualizar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Gravadora atualizado com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async deletar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Gravadora>,
+    res: Response<TResponseDelete | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      await new GravadoraModel(id).deletar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Gravadora deletado com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }

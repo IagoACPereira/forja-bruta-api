4b2e86c4ab447214c41673a31b94686d3aecbb0b
IagoACPereira <iago0307@gmail.com>
2024-11-17 18:35:19 -0300

    feat: ✨ add: Controller para permissões

--- api/controllers/Permissao.controller.ts
+++ api/controllers/Permissao.controller.ts
@@ -1,77 +1,134 @@
 import { Request, Response } from "express";
+import { PaisModel } from "../models/Pais.model";
+import { 
+  TResponseDelete, 
+  TResponseErro, 
+  TResponseGet, 
+  TResponseGetId, 
+  TResponsePost, 
+  TResponsePut 
+} from "../types/Response.type";
+import { TRequestBody, TRequestParams } from "../types/Request.type";
+import { ArtistaModel } from "../models/Artista.model";
+import { TGravadora } from "../types/Gravadora.type";
+import { GravadoraModel } from "../models/Gravadora.model";
+import { TTipo } from "../types/Tipo.type";
+import { TipoModel } from "../models/Tipo.model";
+import { TDisco } from "../types/Disco.type";
+import { DiscoModel } from "../models/Disco.model";
+import { TFaixa } from "../types/Faixa.type";
+import { FaixaModel } from "../models/Faixa.model";
+import { TPermissao } from "../types/Permissao.type";
+import { PermissaoModel } from "../models/Permissao.model";
 
 export class PermissaoController {
   async adicionar(
-    req: Request,
-    res: Response
+    req: Request<{},{},TRequestBody.Permissao>,
+    res: Response<TResponsePost.Permissao | TResponseErro>
   ): Promise<void> {
+    const {
+      descricao,
+      titulo,
+    } = req.body as TPermissao;
     try {
-      res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+      const novaPermissao = await new PermissaoModel(
+        undefined,
+        titulo,
+        descricao,
+      ).adicionar();
+
+      res.status(201).json({
+        mensagem: 'Permissão adicionada com sucesso',
+        dados: novaPermissao,
+        statusCode: 201,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirTodos(
     req: Request,
-    res: Response
+    res: Response<TResponseGet.Permissao | TResponseErro>
   ): Promise<void> {
     try {
+      const permissoes = await new PermissaoModel().pegaTodos();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        permissoes,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirUm(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Permissao>,
+    res: Response<TResponseGetId.Permissao | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      const permissao = await new PermissaoModel(id).pegaUmPorId();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        permissao,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async atualizar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Permissao, {}, TRequestBody.Permissao>,
+    res: Response<TResponsePut | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
+    const {
+      descricao,
+      titulo,
+    } = req.body as TPermissao;
     try {
+      await new PermissaoModel(
+        id,
+        titulo,
+        descricao,
+      ).atualizar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Permissão atualizada com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async deletar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Permissao>,
+    res: Response<TResponseDelete | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      await new PermissaoModel(id).deletar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Permissão deletada com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }

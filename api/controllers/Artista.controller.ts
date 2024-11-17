9f499d97e3dff285c99d8bdac229c2d91bddcfad
IagoACPereira <iago0307@gmail.com>
2024-11-17 17:18:52 -0300

    feat: :sparkles: add: Controller para artistas

--- api/controllers/Artista.controller.ts
+++ api/controllers/Artista.controller.ts
@@ -1,77 +1,141 @@
 import { Request, Response } from "express";
+import { PaisModel } from "../models/Pais.model";
+import { 
+  TResponseDelete, 
+  TResponseErro, 
+  TResponseGet, 
+  TResponseGetId, 
+  TResponsePost, 
+  TResponsePut 
+} from "../types/Response.type";
+import { TRequestBody, TRequestParams } from "../types/Request.type";
+import { TArtista } from "../types/Artista.type";
+import { ArtistaModel } from "../models/Artista.model";
 
 export class ArtistaController {
   async adicionar(
-    req: Request,
-    res: Response
+    req: Request<{},{},TRequestBody.Artista>,
+    res: Response<TResponsePost.Artista | TResponseErro>
   ): Promise<void> {
+    const { 
+      nome,
+      ativo,
+      data_formacao,
+      descricao,
+      id_regiao,
+      url_imagem,
+    } = req.body as TArtista;
     try {
-      res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+      const novoArtista = await new ArtistaModel(
+        undefined,
+        nome,
+        data_formacao,
+        ativo,
+        descricao,
+        url_imagem,
+        id_regiao,
+      ).adicionar();
+
+      res.status(201).json({
+        mensagem: 'Artista adicionado com sucesso',
+        dados: novoArtista,
+        statusCode: 201,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirTodos(
     req: Request,
-    res: Response
+    res: Response<TResponseGet.Artista | TResponseErro>
   ): Promise<void> {
     try {
+      const artistas = await new ArtistaModel().pegaTodos();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        artistas,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirUm(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Artista>,
+    res: Response<TResponseGetId.Artista | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      const artista = await new ArtistaModel(id).pegaUmPorId();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        artista,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async atualizar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Artista, {}, TRequestBody.Artista>,
+    res: Response<TResponsePut | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
+    const { 
+      ativo,
+      data_formacao,
+      descricao,
+      id_regiao,
+      nome,
+      url_imagem,
+     } = req.body as TArtista;
     try {
+      await new ArtistaModel(
+        id, 
+        nome,
+        data_formacao,
+        ativo,
+        descricao,
+        url_imagem,
+        id_regiao,
+      ).atualizar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Artista atualizado com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async deletar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Artista>,
+    res: Response<TResponseDelete | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      await new ArtistaModel(Number(id)).deletar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Artista deletado com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }

bd9b253338d8b000ec4d295f1909961e25bf0766
IagoACPereira <iago0307@gmail.com>
2024-11-17 20:14:59 -0300

    feat: ✨ add: Controller para usuários

--- api/controllers/Usuario.controller.ts
+++ api/controllers/Usuario.controller.ts
@@ -1,77 +1,148 @@
 import { Request, Response } from "express";
+import { PaisModel } from "../models/Pais.model";
+import { 
+  TResponseDelete, 
+  TResponseErro, 
+  TResponseGet, 
+  TResponseGetId, 
+  TResponsePost, 
+  TResponsePut 
+} from "../types/Response.type";
+import { TRequestBody, TRequestParams } from "../types/Request.type";
+import { ArtistaModel } from "../models/Artista.model";
+import { TGravadora } from "../types/Gravadora.type";
+import { GravadoraModel } from "../models/Gravadora.model";
+import { TTipo } from "../types/Tipo.type";
+import { TipoModel } from "../models/Tipo.model";
+import { TDisco } from "../types/Disco.type";
+import { DiscoModel } from "../models/Disco.model";
+import { TFaixa } from "../types/Faixa.type";
+import { FaixaModel } from "../models/Faixa.model";
+import { TPermissao } from "../types/Permissao.type";
+import { PermissaoModel } from "../models/Permissao.model";
+import { TUsuario } from "../types/Usuario.type";
+import { UsuarioModel } from "../models/Usuario.model";
 
 export class UsuarioController {
   async adicionar(
-    req: Request,
-    res: Response
+    req: Request<{},{},TRequestBody.Usuario>,
+    res: Response<TResponsePost.Usuario | TResponseErro>
   ): Promise<void> {
+    const {
+      email,
+      id_permissao,
+      nome,
+      senha,
+      telefone,
+    } = req.body as TUsuario;
     try {
-      res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+      const novoUsuario = await new UsuarioModel(
+        undefined,
+        nome,
+        email,
+        telefone,
+        senha,
+        id_permissao,
+      ).adicionar();
+
+      res.status(201).json({
+        mensagem: 'Usuário adicionado com sucesso',
+        dados: novoUsuario,
+        statusCode: 201,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirTodos(
     req: Request,
-    res: Response
+    res: Response<TResponseGet.Usuario | TResponseErro>
   ): Promise<void> {
     try {
+      const usuarios = await new UsuarioModel().pegaTodos();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        usuarios,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async exibirUm(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Usuario>,
+    res: Response<TResponseGetId.Usuario | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      const usuario = await new UsuarioModel(id).pegaUmPorId();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        usuario,
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async atualizar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Usuario, {}, TRequestBody.Usuario>,
+    res: Response<TResponsePut | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
+    const {
+      email,
+      id_permissao,
+      nome,
+      senha,
+      telefone,
+    } = req.body as TUsuario;
     try {
+      await new UsuarioModel(
+        id,
+        nome,
+        email,
+        telefone,
+        senha,
+        id_permissao,
+      ).atualizar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Usuário atualizado com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }
 
   async deletar(
-    req: Request,
-    res: Response
+    req: Request<TRequestParams.Usuario>,
+    res: Response<TResponseDelete | TResponseErro>
   ): Promise<void> {
+    const { id } = req.params;
     try {
+      await new UsuarioModel(id).deletar();
       res.status(200).json({
-        mensagem: 'Em desenvolvimento',
+        mensagem: 'Usuário deletado com sucesso',
+        statusCode: 200,
       });
     } catch (error) {
       res.status(400).json({
         mensagem: `${error}`,
+        statusCode: 400,
       });
     }
   }

6f52fab3cbee6c87e79d3f03332b97e395728714
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-12-17 11:05:21 -0300

    refactor: :package: remove: Biblioteca knex

--- api/config/conexaoDb.ts
+++ api/config/conexaoDb.ts
@@ -1,19 +1,4 @@
-import _knex, { Knex } from 'knex';
 import { Sequelize } from 'sequelize';
-
-const config: Knex.Config = {
-  client: process.env.CLIENTE_DB,
-  connection: {
-    host: process.env.HOST_DB,
-    port: Number(process.env.PORTA_DB),
-    user: process.env.USER_DB,
-    password: process.env.PASSWORD_DB,
-    database: process.env.DATABASE,
-  },
-  pool: { min: 1, max: 10 },
-};
-
-export const knex = _knex(config);
 
 export const sequelize = new Sequelize(
   String(process.env.DATABASE),
